23a95b857a33a394da9a05e4bce893bee77ae6e8
Ewan Barr <ewan.d.barr@gmail.com>
2024-04-24 13:13:51 +0200

    set up working dirs for the tests

--- cpp/skyweaver/test/DelayManagerTester.cuh
+++ cpp/skyweaver/test/DelayManagerTester.cuh
@@ -2,26 +2,29 @@
 #define SKYWEAVER_TEST_DELAYMANAGERTESTER_CUH
 
 #include "skyweaver/DelayManager.cuh"
+
 #include <gtest/gtest.h>
 
-namespace skyweaver {
-namespace test {
+namespace skyweaver
+{
+namespace test
+{
 
 class DelayManagerTester: public ::testing::Test
 {
-protected:
+  protected:
     void SetUp() override;
     void TearDown() override;
 
-public:
+  public:
     DelayManagerTester();
     ~DelayManagerTester();
 
-protected:
+  protected:
     cudaStream_t _stream;
 };
 
-} //namespace test
-} //namespace skyweaver
+} // namespace test
+} // namespace skyweaver
 
-#endif //SKYWEAVER_TEST_DELAYMANAGERTESTER_CUH
+#endif // SKYWEAVER_TEST_DELAYMANAGERTESTER_CUH
--- cpp/skyweaver/test/src/DelayManagerTester.cu
+++ cpp/skyweaver/test/src/DelayManagerTester.cu
@@ -2,6 +2,9 @@
 #include "skyweaver/test/DelayManagerTester.cuh"
 #include "thrust/host_vector.h"
 
+#include <cstdlib>
+#include <iostream>
+
 namespace skyweaver
 {
 namespace test
@@ -17,22 +20,36 @@ DelayManagerTester::~DelayManagerTester()
 
 void DelayManagerTester::SetUp()
 {
-  CUDA_ERROR_CHECK(cudaStreamCreate(&_stream));
+    CUDA_ERROR_CHECK(cudaStreamCreate(&_stream));
 }
 
 void DelayManagerTester::TearDown()
 {
-  CUDA_ERROR_CHECK(cudaStreamDestroy(_stream));
+    CUDA_ERROR_CHECK(cudaStreamDestroy(_stream));
 }
+
+/**
+ * The data/ directory contains a test delay file called
+ * test_delays.bin.
+ *
+ * It contains 5 delay models for 67 beams and 57 antennas.
+ *
+ * The first valid epoch is   1708082168.957
+ * The last valid epoch is 1708082468.957
+ *
+ * delay offsets are in the range 1e-7 to 1e-10
+ * delay rates are in the range 1e-12 to 1e-14
+ * weights are either 1 or 0
+ */
 
 TEST_F(DelayManagerTester, test_valid_read)
 {
-  // TODO
+    DelayManager delay_manager("data/test_delays.bin", _stream);
 }
 
 TEST_F(DelayManagerTester, test_invalid_read)
 {
-  // TODO
+    // TODO
 }
 
 } // namespace test

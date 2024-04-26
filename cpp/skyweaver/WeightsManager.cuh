5d6316fc8ec74762a339d70f974940522149f21e
Ewan Barr <ewan.d.barr@gmail.com>
2024-04-26 17:14:30 +0200

    added weights generation

---
+++ cpp/skyweaver/WeightsManager.cuh
@@ -0,0 +1,79 @@
+#ifndef SKYWEAVER_WEIGHTSMANAGER_CUH
+#define SKYWEAVER_WEIGHTSMANAGER_CUH
+
+#include "skyweaver/PipelineConfig.hpp"
+#include "skyweaver/DelayManager.cuh"
+#include <thrust/device_vector.h>
+
+namespace skyweaver
+{
+namespace kernels
+{
+
+__global__ void
+generate_weights_k(float3 const* __restrict__ delay_models,
+                   char2* __restrict__ weights,
+                   double const* __restrict__ channel_frequencies,
+                   int nantennas,
+                   int nbeams,
+                   int nchans,
+                   double tstart,
+                   double tstep,
+                   int ntsteps);
+
+} // namespace kernels
+
+class WeightsManager
+{
+  public:
+    typedef char2 WeightsType;
+    typedef thrust::device_vector<WeightsType> WeightsVectorType;
+    typedef double FreqType;
+    typedef thrust::device_vector<FreqType> FreqVectorType;
+    typedef double TimeType;
+    typedef DelayManager::DelayVectorDType DelayVectorType;
+
+  public:
+    /**
+     * @brief      Create a new weights mananger object
+     *
+     * @param      config          The pipeline configuration
+     */
+    WeightsManager(PipelineConfig const& config, cudaStream_t stream);
+    ~WeightsManager();
+    WeightsManager(WeightsManager const&) = delete;
+
+    /**
+     * @brief      Calculate beamforming weights for a given epock
+     *
+     * @param[in]  delays A vector containing delay models for all beams
+     * (produced by an instance of DelayManager)
+     *
+     * @param[in]  epoch  The epoch at which to evaluate the given delay models
+     *
+     * @detail     No check is performed here on whether the provided epoch is
+     * in the bounds of the current delay polynomial. The assumption here is the
+     * we are running real time and as such there is no large latency between
+     * the reception of a set of delay models and a request for weights from
+     * that model. If this is not the case a more sophisticated mechanism for
+     * control of the delays into the beamformer will have to be developed.
+     *
+     * @note       This function is not thread-safe!!! Competing calls will
+     * overwrite the memory of the _weights object.
+     *
+     * @return     A thrust device vector containing the generated weights
+     */
+    WeightsVectorType const& weights(DelayVectorType const& delays,
+                                     TimeType current_epoch,
+                                     TimeType delay_epoch);
+
+  private:
+    PipelineConfig const& _config;
+    cudaStream_t _stream;
+    WeightsVectorType _weights;
+    FreqVectorType _channel_frequencies;
+};
+
+} // namespace skyweaver
+
+#endif // SKYWEAVER_WEIGHTSMANAGER_CUH

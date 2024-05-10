6b4c5db6cfcf464dd571e09becba0da3525ac570
Ewan Barr <ewan.d.barr@gmail.com>
2024-05-10 12:45:52 +0200

    added transposer code

---
+++ cpp/skyweaver/Transposer.cuh
@@ -0,0 +1,76 @@
+#ifndef SKYWEAVER_TRANSPOSER_HPP
+#define SKYWEAVER_TRANSPOSER_HPP
+
+#include "psrdada_cpp/common.hpp"
+#include "skyweaver/PipelineConfig.hpp"
+#include "thrust/device_vector.h"
+
+namespace skyweaver
+{
+namespace kernel
+{
+
+/**
+ * @brief      Perform a split transpose of input data
+ *             in TAFTP order.
+ *
+ * @param      input            8-bit complex voltage data in TAFTP order
+ * @param      output           8-bit complex voltage data in FTPA order
+ * @param[in]  total_nantennas  The total number of antennas (e.g. T[A]FTP)
+ * @param[in]  input_antennas   The number of antennas in the input data
+ * @param[in]  output_antennas  The number of antennas to pad to in the output
+ *                              (0 valued antennas used for padding)
+ * @param[in]  nchans           The number of frequency channels (e.g. TA[F]TP)
+ * @param[in]  ntimestamps      The number of timestamps (outer T dimension,
+ * e.g. [T]AFTP)
+ */
+__global__ void split_transpose_k(char2 const* __restrict__ input,
+                                  char2* __restrict__ output,
+                                  int input_antennas,
+                                  int output_antennas,
+                                  int start_antenna,
+                                  int nchans,
+                                  int ntimestamps);
+
+} // namespace kernel
+
+/**
+ * @brief      Class for split transposing voltage data
+ *
+ */
+class Transposer
+{
+  public:
+    typedef thrust::device_vector<char2> VoltageType;
+
+  public:
+    /**
+     * @brief      Create a new split transposer
+     *
+     * @param      config  The pipeline configuration
+     */
+    explicit Transposer(PipelineConfig const& config);
+    ~Transposer();
+    Transposer(Transposer const&) = delete;
+
+    /**
+     * @brief      Perform a split transpose on the data
+     *
+     * @param      taftp_voltages  The input TAFTP voltages
+     * @param      ftpa_voltages   The output FTPA voltages
+     * @param[in]  input_nantennas The number of antennas in the input data
+     * @param[in]  stream          The cuda stream to use
+     */
+    void transpose(VoltageType const& taftp_voltages,
+                   VoltageType& ftpa_voltages,
+                   std::size_t input_nantennas,
+                   cudaStream_t stream);
+
+  private:
+    PipelineConfig const& _config;
+    std::size_t _output_size_per_heap_group;
+};
+
+} // namespace skyweaver
+
+#endif // SKYWEAVER_TRANSPOSER_HPP
---
+++ cpp/skyweaver/src/Transposer.cu
@@ -0,0 +1,162 @@
+#include "psrdada_cpp/cuda_utils.hpp"
+#include "skyweaver/Transposer.cuh"
+
+#define SKYWEAVER_ST_MAX_ANTENNAS 32
+
+namespace skyweaver
+{
+namespace kernels
+{
+
+__global__ void transpose_k(char2 const* __restrict__ input,
+                            char2* __restrict__ output,
+                            int input_nantennas,
+                            int output_nantennas,
+                            int nchans,
+                            int ntimestamps)
+{
+    // Requires output_nantennas >= input_nantennas
+    __shared__ char2 transpose_buffer[SKYWEAVER_ST_MAX_ANTENNAS]
+                                     [SKYWEAVER_NSAMPLES_PER_HEAP][SKYWEAVER_NPOL];
+
+    // TAFTP (input dimensions)
+    const int tp   = SKYWEAVER_NSAMPLES_PER_HEAP * SKYWEAVER_NPOL;
+    const int ftp  = nchans * tp;
+    const int aftp = input_nantennas * ftp;
+
+    // FTPA
+    const int pa  = SKYWEAVER_NPOL * output_nantennas;
+    const int tpa = ntimestamps * SKYWEAVER_NSAMPLES_PER_HEAP * pa;
+
+    int nantennas_sets =
+        ceilf(((float)input_nantennas) / SKYWEAVER_ST_MAX_ANTENNAS);
+
+    // Each timestamp here is a heap timestamp so 256 samples
+    for(int timestamp_idx = blockIdx.x; timestamp_idx < ntimestamps;
+        timestamp_idx += gridDim.x) {
+        // Each channel is a standard frequency channel
+        for(int chan_idx = blockIdx.y; chan_idx < nchans;
+            chan_idx += gridDim.y) {
+            // We load antennas in groups of 32 for efficiency, each set is up
+            // to 32 antennas
+            for(int antenna_set_idx = 0; antenna_set_idx < nantennas_sets;
+                ++antenna_set_idx) {
+                // Here calculate the number of antennas in the current set
+                int remaining_antennas = min(
+                    input_nantennas - antenna_set_idx * SKYWEAVER_ST_MAX_ANTENNAS,
+                    SKYWEAVER_ST_MAX_ANTENNAS);
+
+                // Load antenna data into shared memory
+                for(int antenna_idx = threadIdx.y;
+                    antenna_idx < remaining_antennas;
+                    antenna_idx += blockDim.y) {
+                    int input_antenna_idx =
+                        antenna_set_idx * SKYWEAVER_ST_MAX_ANTENNAS + antenna_idx;
+
+                    // Loop over the TP samples perforing a coalesced read from
+                    // global memory and a coalesced write to shared memory
+                    for(int samppol_idx = threadIdx.x;
+                        samppol_idx < (SKYWEAVER_NSAMPLES_PER_HEAP * SKYWEAVER_NPOL);
+                        samppol_idx += blockDim.x) {
+                        int pol_idx  = samppol_idx % SKYWEAVER_NPOL;
+                        int samp_idx = samppol_idx / SKYWEAVER_NPOL;
+                        int input_idx =
+                            timestamp_idx * aftp + input_antenna_idx * ftp +
+                            chan_idx * tp + samp_idx * SKYWEAVER_NPOL + pol_idx;
+                        transpose_buffer[antenna_idx][samp_idx][pol_idx] =
+                            input[input_idx];
+                    }
+                }
+                __syncthreads();
+                for(int pol_idx = 0; pol_idx < SKYWEAVER_NPOL; ++pol_idx) {
+                    for(int samp_idx = threadIdx.y;
+                        samp_idx < SKYWEAVER_NSAMPLES_PER_HEAP;
+                        samp_idx += blockDim.y) {
+                        int output_sample_idx =
+                            samp_idx + timestamp_idx * SKYWEAVER_NSAMPLES_PER_HEAP;
+                        for(int antenna_idx = threadIdx.x;
+                            antenna_idx < remaining_antennas;
+                            antenna_idx += blockDim.x) {
+                            int output_antenna_idx =
+                                antenna_set_idx * SKYWEAVER_ST_MAX_ANTENNAS +
+                                antenna_idx;
+                            // FTPA
+                            int output_idx =
+                                chan_idx * tpa + output_sample_idx * pa +
+                                pol_idx * output_nantennas + output_antenna_idx;
+                            output[output_idx] =
+                                transpose_buffer[antenna_idx][samp_idx]
+                                                [pol_idx];
+                        }
+                    }
+                }
+                __syncthreads();
+            }
+        }
+    }
+}
+
+} // namespace kernels
+
+Transposer::Transposer(PipelineConfig const& config)
+    : _config(config), _output_size_per_heap_group(0)
+{
+    BOOST_LOG_TRIVIAL(debug) << "Constructing Transposer instance";
+    _output_size_per_heap_group =
+        (_config.npol() * _config.nsamples_per_heap() * _config.nchans() *
+         _config.nantennas());
+    BOOST_LOG_TRIVIAL(debug)
+        << "Output size per heap group: " << _output_size_per_heap_group;
+}
+
+Transposer::~Transposer()
+{
+}
+
+void Transposer::transpose(VoltageType const& taftp_voltages,
+                           VoltageType& ftpa_voltages,
+                           std::size_t input_nantennas,
+                           cudaStream_t stream)
+{
+    BOOST_LOG_TRIVIAL(debug) << "Performing split transpose";
+
+    std::size_t heap_group_size = (_config.npol() * _config.nsamples_per_heap() *
+                        _config.nchans() * input_nantennas);
+
+
+    // Check sizes
+    assert(taftp_voltages.size() % heap_group_size == 0);
+    int nheap_groups = taftp_voltages.size() / heap_group_size;
+    BOOST_LOG_TRIVIAL(debug) << "Number of heap groups: " << nheap_groups;
+    // Resize output buffer
+    BOOST_LOG_TRIVIAL(debug)
+        << "Resizing output buffer from " << ftpa_voltages.size() << " to "
+        << _output_size_per_heap_group * nheap_groups;
+    // Resize the output and set the data to zero
+    // _output_size_per_heap_group contains the number of antennas to pad to
+    ftpa_voltages.resize(_output_size_per_heap_group * nheap_groups, {0, 0});
+    dim3 grid(nheap_groups, _config.nchans(), 1);
+    dim3 block(512, 1, 1);
+    char2 const* input_ptr = thrust::raw_pointer_cast(taftp_voltages.data());
+    char2* output_ptr      = thrust::raw_pointer_cast(ftpa_voltages.data());
+    BOOST_LOG_TRIVIAL(debug)
+        << "Transposing and padding TAFTP (" << nheap_groups << ", "
+        << input_nantennas << ", " << _config.nchans() << ", "
+        << _config.nsamples_per_heap() << ", " << _config.npol()
+        << ") to FTPA (" << _config.nchans() << ", "
+        << _config.nsamples_per_heap() * nheap_groups << ", " << _config.npol()
+        << _config.nantennas() << ")";
+    BOOST_LOG_TRIVIAL(debug) << "Launching split transpose kernel";
+    kernels::transpose_k<<<grid, block, 0, stream>>>(
+        input_ptr,
+        output_ptr,
+        input_nantennas,
+        _config.nantennas(),
+        _config.nchans(),
+        nheap_groups);
+    // Not sure if this should be here, will check later
+    CUDA_ERROR_CHECK(cudaStreamSynchronize(stream));
+    BOOST_LOG_TRIVIAL(debug) << "Split transpose complete";
+}
+
+} // namespace skyweaver

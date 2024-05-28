a44390b101b0f2780a79f82f1c6abdce5367b701
Ewan Barr <ewan.d.barr@gmail.com>
2024-05-28 09:37:03 +0200

    changed beamformer bench to a fixture

--- cpp/skyweaver/benchmark/beamformer_bench.cu
+++ cpp/skyweaver/benchmark/beamformer_bench.cu
@@ -5,120 +5,102 @@
 #include "skyweaver/PipelineConfig.hpp"
 #include "skyweaver/skyweaver_constants.hpp"
 
-#include <cuda.h>
 #include <benchmark/benchmark.h>
 #include <chrono>
 #include <ctime>
+#include <cuda.h>
 #include <iostream>
 #include <thread>
-static void BM_CustomCounter(benchmark::State& state)
-{
-    int64_t num_operations = 0;
 
-    for(auto _: state) {
-        // Simulate some work
-        for(int i = 0; i < state.range(0); ++i) {
-            ++num_operations;
-            benchmark::DoNotOptimize(num_operations);
-        }
+class BeamformerBencher: public benchmark::Fixture
+{
+  public:
+    void SetUp(::benchmark::State const& state)
+    {
+        std::size_t ntimestamps = state.range(0); // ntimestamps
+        std::size_t input_size =
+            (ntimestamps * _config.nantennas() * _config.nchans() *
+             _config.nsamples_per_heap() * _config.npol());
+        std::size_t weights_size =
+            _config.nantennas() * _config.nchans() * _config.nbeams();
+        ftpa_voltages_gpu.resize(input_size);
+        fbpa_weights_gpu.resize(weights_size);
+        scales.resize(_config.nchans());
+        offsets.resize(_config.nchans());
+        CUDA_ERROR_CHECK(cudaStreamCreate(&_stream));
+        CUDA_ERROR_CHECK(cudaEventCreate(&start));
+        CUDA_ERROR_CHECK(cudaEventCreate(&stop));
     }
 
-    // Use counter to report the number of operations
-    state.counters["Operations"] = benchmark::Counter(num_operations);
-    state.counters["Operations/Second"] =
-        benchmark::Counter(num_operations, benchmark::Counter::kIsRate);
-}
-
-BENCHMARK(BM_CustomCounter)->ArgsProduct({{1000, 2000, 3000}})->MinTime(1.0);
-
-void BM_LOOP2(benchmark::State& state)
-{
-    int n = state.range(0);
-    int z = 0;
-    for(auto _: state) {
-        for(int ii = 0; ii < n; ++ii) { z += ii; }
-        benchmark::DoNotOptimize(z);
+    void TearDown(::benchmark::State const& state)
+    {
+        CUDA_ERROR_CHECK(cudaStreamDestroy(_stream));
+        CUDA_ERROR_CHECK(cudaEventDestroy(start));
+        CUDA_ERROR_CHECK(cudaEventDestroy(stop));
     }
-    state.counters["rate"] = benchmark::Counter(n, benchmark::Counter::kIsRate);
-    state.counters["n"]    = benchmark::Counter(n);
-}
 
-BENCHMARK(BM_LOOP2)
-    ->ArgsProduct({{10, 100, 1000, 10000, 100000}})
-    ->Iterations(3000);
-
-void BM_LOOP(benchmark::State& state)
-{
-    int n = state.range(0);
-    for(auto _: state) {
-        auto start = std::chrono::high_resolution_clock::now();
-        std::this_thread::sleep_for(std::chrono::milliseconds(n));
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> time_span =
-            std::chrono::duration_cast<std::chrono::duration<double>>(end -
-                                                                      start);
-        state.SetIterationTime(time_span.count());
+    // Implemented to avoid warning #611-D
+    void SetUp(::benchmark::State& state)
+    {
+        SetUp(const_cast<const ::benchmark::State&>(state));
     }
-    state.counters["rate"] =
-        benchmark::Counter(n, benchmark::Counter::kAvgIterationsRate);
-    state.counters["n"] = n;
-}
+    void TearDown(::benchmark::State& state)
+    {
+        TearDown(const_cast<const ::benchmark::State&>(state));
+    }
 
-BENCHMARK(BM_LOOP)
-    ->ArgsProduct({{10, 100, 1000}})
-    ->Iterations(3)
-    ->UseManualTime();
-
-void BM_COHERENT_BEAMFORMER(benchmark::State& state)
-{
+  public:
     cudaStream_t _stream;
-    CUDA_ERROR_CHECK(cudaStreamCreate(&_stream));
+    cudaEvent_t start, stop;
     skyweaver::PipelineConfig _config;
-    skyweaver::CoherentBeamformer coherent_beamformer(_config);
-    skyweaver::IncoherentBeamformer incoherent_beamformer(_config);
-    std::size_t ntimestamps = max(
-        1L,
-        SKYWEAVER_CB_PACKET_SIZE / (_config.nchans() / _config.cb_fscrunch()) /
-            (_config.nsamples_per_heap() / _config.cb_tscrunch()));
-    ntimestamps =
-        max(ntimestamps,
-            SKYWEAVER_CB_NSAMPLES_PER_BLOCK / _config.nsamples_per_heap());
-    std::size_t input_size =
-        (ntimestamps * _config.nantennas() * _config.nchans() *
-         _config.nsamples_per_heap() * _config.npol());
-    std::size_t weights_size =
-        _config.nantennas() * _config.nchans() * _config.nbeams();
-    skyweaver::CoherentBeamformer::VoltageVectorType ftpa_voltages_gpu(
-        input_size);
-    skyweaver::CoherentBeamformer::WeightsVectorType fbpa_weights_gpu(
-        weights_size);
+    skyweaver::CoherentBeamformer::VoltageVectorType ftpa_voltages_gpu;
+    skyweaver::CoherentBeamformer::WeightsVectorType fbpa_weights_gpu;
     skyweaver::CoherentBeamformer::PowerVectorType btf_powers_gpu;
-    skyweaver::CoherentBeamformer::ScalingVectorType scales(
-        _config.nchans());
-    skyweaver::CoherentBeamformer::ScalingVectorType offsets(
-        _config.nchans());
+    skyweaver::CoherentBeamformer::ScalingVectorType scales;
+    skyweaver::CoherentBeamformer::ScalingVectorType offsets;
     skyweaver::CoherentBeamformer::PowerVectorType tf_powers_gpu;
     skyweaver::CoherentBeamformer::RawPowerVectorType tf_powers_raw_gpu;
+};
+
+BENCHMARK_DEFINE_F(BeamformerBencher, simple_bench)(benchmark::State& state)
+{
+    typedef skyweaver::CoherentBeamformer::VoltageVectorType::value_type ValueType;
+    float elapsed_time;
+    float total_elapsed_time = 0.0f;
+    skyweaver::CoherentBeamformer coherent_beamformer(_config);
+    skyweaver::IncoherentBeamformer incoherent_beamformer(_config);
     for(auto _: state) {
-        incoherent_beamformer.beamform(ftpa_voltages_gpu,
-                                       tf_powers_raw_gpu,
-                                       tf_powers_gpu,
-                                       scales,
-                                       offsets,
-                                       _stream);
-        coherent_beamformer.beamform(ftpa_voltages_gpu,
-                                     fbpa_weights_gpu,
-                                     scales,
-                                     offsets,
-                                     tf_powers_raw_gpu,
-                                     btf_powers_gpu,
-                                     _stream);
+        CUDA_ERROR_CHECK(cudaEventRecord(start, 0));
+        for(int ii = 0; ii < 10; ++ii) {
+            incoherent_beamformer.beamform(ftpa_voltages_gpu,
+                                           tf_powers_raw_gpu,
+                                           tf_powers_gpu,
+                                           scales,
+                                           offsets,
+                                           _stream);
+            coherent_beamformer.beamform(ftpa_voltages_gpu,
+                                         fbpa_weights_gpu,
+                                         scales,
+                                         offsets,
+                                         tf_powers_raw_gpu,
+                                         btf_powers_gpu,
+                                         _stream);
+        }
         CUDA_ERROR_CHECK(cudaStreamSynchronize(_stream));
+        CUDA_ERROR_CHECK(cudaEventRecord(stop, 0));
+        CUDA_ERROR_CHECK(cudaEventSynchronize(stop));
+        CUDA_ERROR_CHECK(cudaEventElapsedTime(&elapsed_time, start, stop));
+        state.SetIterationTime(elapsed_time);
+        total_elapsed_time += elapsed_time;
     }
-    CUDA_ERROR_CHECK(cudaStreamDestroy(_stream));
+    state.counters["Ntimestamps"] = state.range(0);
+    float performance =
+        (ftpa_voltages_gpu.size() * sizeof(ValueType) * state.iterations() * 10) /
+        total_elapsed_time / 1e9;
+    state.counters["Performance GB/s"] = performance;
 }
 
-BENCHMARK(BM_COHERENT_BEAMFORMER)->Iterations(30);
+BENCHMARK_REGISTER_F(BeamformerBencher, simple_bench)->Args({1, 10, 100})->Iterations(30);
 
 int main(int argc, char** argv)
 {

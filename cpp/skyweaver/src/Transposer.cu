66d0de14c297aa2d928c5eaf23834593ea12251a
Ewan Barr <ewan.d.barr@gmail.com>
2024-06-03 13:04:35 +0200

    logging formatting

--- cpp/skyweaver/src/Transposer.cu
+++ cpp/skyweaver/src/Transposer.cu
@@ -121,9 +121,10 @@ void Transposer::transpose(VoltageType const& taftp_voltages,
     BOOST_LOG_TRIVIAL(debug) << "Performing split transpose";
 
     std::size_t heap_group_size = (_config.npol() * _config.nsamples_per_heap() *
-                        _config.nchans() * input_nantennas);
+                                   _config.nchans() * input_nantennas);
 
     // Check sizes
+    BOOST_LOG_TRIVIAL(debug) << "Transpose input size: " << taftp_voltages.size();
     if (taftp_voltages.size() % heap_group_size != 0)
     {
         throw std::runtime_error("Voltages are not a multiple of the heap size");
@@ -150,7 +151,8 @@ void Transposer::transpose(VoltageType const& taftp_voltages,
         << input_nantennas << ", " << _config.nchans() << ", "
         << _config.nsamples_per_heap() << ", " << _config.npol()
         << ") to FTPA (" << _config.nchans() << ", "
-        << _config.nsamples_per_heap() * nheap_groups << ", " << _config.npol()
+        << _config.nsamples_per_heap() * nheap_groups << ", " 
+        << _config.npol() << ", "
         << _config.nantennas() << ")";
     BOOST_LOG_TRIVIAL(debug) << "Launching split transpose kernel";
     kernels::transpose_k<<<grid, block, 0, stream>>>(

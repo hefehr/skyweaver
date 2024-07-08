e37017957b41cf11d0b1f46ed04bf4c05616df52
Ewan Barr <ebarr@contrapn101.compact.mpifr-bonn.mpg.de>
2024-07-08 16:58:32 +0200

    first pass on dedisperison utils in one file

---
+++ cpp/skyweaver/dedispersion_utils.cuh
@@ -0,0 +1,46 @@
+#ifndef SKYWEAVER_DEDISPERSION_UTILS_CUH
+#define SKYWEAVER_DEDISPERSION_UTILS_CUH
+
+namespace skyweaver {
+
+template <typename T> static constexpr T pi = static_cast<T>( acos(-1.0));
+template <typename T> static constexpr T two_pi = static_cast<T>(2 * pi<T>);
+template <typename T> static constexpr T dm_const_hz_s = static_cast<T>(4.148806423e15); // Hz^2 pc^-1 cm^3 s
+template <typename T> static constexpr T dm_const_mhz_us = static_cast<T>(4.148806423e9); // MHz^2 pc^-1 cm^3 us
+
+
+// Freq in Hz!
+template <typename T>
+__host__ __device__
+T dm_delay(T f_ref, T f, T dm)
+{
+    return static_cast<T>(dm_const_hz_s<T> * (1/(f_ref * f_ref) - 1/(f * f)) * dm);
+}
+
+struct DMSampleDelay
+{
+    __host__ __device__
+    DMDelay(double dm, double f_ref, double tsamp)
+    : _dm(dm), _f_ref(f_ref), _tsamp(tsamp){}
+
+    __host__ __device__
+    int operator()(double const& freq){
+        return static_cast<int>(__double2ll_rn(dm_delay<double>(_f_ref, freq, _dm)/ _tsamp));
+    }
+
+    double _dm;
+    double _f_ref; // reference frequency Hz
+    double _tsamp; // sampling interval seconds
+};
+
+struct DMPrefactor
+{
+    __host__ __device__
+    int operator()(double const& dm){
+        return -1.0f * two_pi<double> * dm_const_mhz_us<double> * dm; 
+    }
+};
+
+}
+
+#endif //SKYWEAVER_DEDISPERSION_UTILS_CUH

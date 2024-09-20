da09af62b03d2fbb844132a6162505ee8ee38dfa
Henning Fehrmann <henning.fehrmann@aei.mpg.de>
2024-09-20 13:09:51 +0000

    define a private call back function

--- cpp/skyweaver/MultiFileWriter.cuh
+++ cpp/skyweaver/MultiFileWriter.cuh
@@ -139,6 +139,7 @@ class MultiFileWriter
     std::string get_extension(VectorType const& stream_data);
     CreateStreamCallBackType _create_stream_callback;
     MultiFileWriterConfig _config;
+    PreWriteCallback _pre_write_callback;
     std::string _tag;
     ObservationHeader _header;
     std::map<std::size_t, std::unique_ptr<FileOutputStream>> _file_streams;
